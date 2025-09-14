02b7c1b0b03dd95a3b38a6a79cb32895e0d9deb5
Anna Eywa <annaeywa@Annas-MacBook-Air.local>
2025-09-13 17:57:08 -0700

    Create and start server with the FE

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+//? Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+//? Get the directory name from the file path
+const __dirname = dirname(__filename);
+
+/* MIDDLEWARE */
+app.use(express.json());
+
+//? Tells express to serve all files from the public folder as static assets / file. Any requests for the css files will be resolved to the public directory.
+app.use(express.static(path.join(__dirname, "../public")));
+//? Serving up the HTML file from the /public directory
+app.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port: ${PORT}`);
+});
